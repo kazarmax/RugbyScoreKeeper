f0369bdaae4a70fbaf51fca380482718b5d39150
Максим Казарцев <kazarmax@MacBook-Pro-Maksim.local>
2016-12-02 08:20:55 +0300

    add app files

---
+++ app/src/main/java/com/example/android/rugbyscorekeeper/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.android.rugbyscorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Team teamA, teamB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        teamA = new Team(R.id.scoreA, R.id.tryCountA, R.id.conversionCountA,
+                R.id.penaltyCountA, R.id.dropGoalCountA);
+
+        teamB = new Team(R.id.scoreB, R.id.tryCountB, R.id.conversionCountB,
+                R.id.penaltyCountB, R.id.dropGoalCountB);
+    }
+
+    public void updateScoresAndStatsForTeam(View v) {
+        switch (v.getId()) {
+            case R.id.tryButtonA:
+                teamA.incrementTryCount();
+                displayStatsForTeam(teamA);
+                break;
+
+            case R.id.tryButtonB:
+                teamB.incrementTryCount();
+                displayStatsForTeam(teamB);
+                break;
+
+            case R.id.conversionButtonA:
+                teamA.incrementConversionCount();
+                displayStatsForTeam(teamA);
+                break;
+
+            case R.id.conversionButtonB:
+                teamB.incrementConversionCount();
+                displayStatsForTeam(teamB);
+                break;
+
+            case R.id.penaltyButtonA:
+                teamA.incrementPenaltyCount();
+                displayStatsForTeam(teamA);
+                break;
+
+            case R.id.penaltyButtonB:
+                teamB.incrementPenaltyCount();
+                displayStatsForTeam(teamB);
+                break;
+
+            case R.id.dropGoalButtonA:
+                teamA.incrementDropGoalCount();
+                displayStatsForTeam(teamA);
+                break;
+
+            case R.id.dropGoalButtonB:
+                teamB.incrementDropGoalCount();
+                displayStatsForTeam(teamB);
+                break;
+        }
+    }
+
+    public void resetScores(View v) {
+        teamA.resetScores();
+        teamB.resetScores();
+        displayStatsForTeam(teamA);
+        displayStatsForTeam(teamB);
+    }
+
+    private void displayStatsForTeam(Team team) {
+        TextView scoreView = (TextView) findViewById(team.getScoreTextViewId());
+        scoreView.setText(String.valueOf(team.getScore()));
+
+        TextView tryCountView = (TextView) findViewById(team.getTryCountTextViewId());
+        tryCountView.setText(String.valueOf(team.getTryCount()));
+
+        TextView conversionCountView = (TextView) findViewById(team.getConversionCountTextViewId());
+        conversionCountView.setText(String.valueOf(team.getConversionCount()));
+
+        TextView penaltyCountView = (TextView) findViewById(team.getPenaltyCountTextViewId());
+        penaltyCountView.setText(String.valueOf(team.getPenaltyCount()));
+
+        TextView dropGoalCountView = (TextView) findViewById(team.getDropGoalCountTextViewId());
+        dropGoalCountView.setText(String.valueOf(team.getDropGoalCount()));
+    }
+
+}
---
+++ app/src/main/java/com/example/android/rugbyscorekeeper/Team.java
@@ -0,0 +1,99 @@
+package com.example.android.rugbyscorekeeper;
+
+public class Team {
+
+    private static final int TRY_POINTS = 5;
+    private static final int CONVERSION_POINTS = 2;
+    private static final int PENALTY_POINTS = 3;
+    private static final int DROP_GOAL_POINTS = 3;
+
+    private final int scoreTextViewId;
+    private final int tryCountTextViewId;
+    private final int conversionCountTextViewId;
+    private final int penaltyCountTextViewId;
+    private final int dropGoalCountTextViewId;
+
+    private int score = 0;
+    private int tryCount = 0;
+    private int conversionCount = 0;
+    private int penaltyCount = 0;
+    private int dropGoalCount = 0;
+
+    public Team(int scoreTextViewId, int tryCountTextViewId, int conversionCountTextViewId,
+                int penaltyCountTextViewId, int dropGoalCountTextViewId) {
+        this.scoreTextViewId = scoreTextViewId;
+        this.tryCountTextViewId = tryCountTextViewId;
+        this.conversionCountTextViewId = conversionCountTextViewId;
+        this.penaltyCountTextViewId = penaltyCountTextViewId;
+        this.dropGoalCountTextViewId = dropGoalCountTextViewId;
+    }
+
+    public int getScoreTextViewId() {
+        return scoreTextViewId;
+    }
+
+    public int getTryCountTextViewId() {
+        return tryCountTextViewId;
+    }
+
+    public int getConversionCountTextViewId() {
+        return conversionCountTextViewId;
+    }
+
+    public int getPenaltyCountTextViewId() {
+        return penaltyCountTextViewId;
+    }
+
+    public int getDropGoalCountTextViewId() {
+        return dropGoalCountTextViewId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public int getConversionCount() {
+        return conversionCount;
+    }
+
+    public int getPenaltyCount() {
+        return penaltyCount;
+    }
+
+    public int getDropGoalCount() {
+        return dropGoalCount;
+    }
+
+    public void incrementTryCount() {
+        tryCount += 1;
+        score += TRY_POINTS;
+    }
+
+    public void incrementConversionCount() {
+        conversionCount += 1;
+        score += CONVERSION_POINTS;
+    }
+
+    public void incrementPenaltyCount() {
+        penaltyCount += 1;
+        score += PENALTY_POINTS;
+    }
+
+    public void incrementDropGoalCount() {
+        dropGoalCount += 1;
+        score += DROP_GOAL_POINTS;
+    }
+
+    public void resetScores() {
+        score = 0;
+        tryCount = 0;
+        conversionCount = 0;
+        penaltyCount = 0;
+        dropGoalCount = 0;
+    }
+
+}
